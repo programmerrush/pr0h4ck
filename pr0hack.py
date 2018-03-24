9f8977fa24bddd24315a9b4462d02f6923e87d16
Programmer Rushikesh <37672619+programmerrush@users.noreply.github.com>
2018-03-25 00:55:03 +0530

    Update pr0hack.py

--- pr0hack.py
+++ pr0hack.py
@@ -8,30 +8,26 @@ import sys
 d3=os.system("curl http://ipinfo.io/ip")
 os.system("clear && clear && clear")
 logo = '''\033[0m 
-  mmerRu                                                                              mmerRu                    ra       
-  rogramm                                                                             rogramm                   hP       
-  Ru   ro                                                                             Ru   ro                   er       
-  am   Ru  Prog  mmerRu  Progra   rRus  rogra  erRushProg   merRushPro  ammerR   Prog am   Ru   ro  am  rRushP  gramme   
-  Program  rR s  ro  am  rR  hP   ra m     hP  gr  me  us   og  mm  Ru  Pr  ra   rR s Program   Ru  Pr  ra      sh  og   
-  rRushP   ra    Ru  Pr  ra  er   hP    ammer  sh  og  mm   us  ro  am  rRushP   ra   rRushP    am  rR  hProg   me  us   
-  ra       hP    am  rR  hP  gr   er   hProgr  me  us  ro   mm  Ru  Pr  ramme    hP   ra  er    Pr  ra   rRush  og  mm   
-  hP       er    Pr  ra  er  sh   gr   er  sh  og  mm  Ru   ro  am  rR  hP       er   hP   ra   rR  hP      me  us  ro   
-  er       gr     Rush    ramme   sh    ramme  us  ro  am   Ru  Pr  ra   rRush   gr   er   hP    ammer  shPro   mm  Ru   
-                             og                                                                                          
-                         merRus 
-                         
-           Pr0               4             4          r                       
-     0#4ckPr0#          Pr0#4c       kP 0#        ckP              r0#      
-     r0#   Pr0  ck r0  ckP 0#4c     4ckPr0#4    0#4ck       Pr0#   Pr0 4ck  
-     Pr0  ckPr #4ckPr #4c  r0#4    0#4 kPr     Pr #4c      ckPr0  ckP 0#4c  
-    ckPr #4ck  0#4c   0#4  Pr0#    r0# ckPr   ck  0#4     #4c     4ckPr0#   
-    4ckPr0#4c  r0#   Pr0#  kPr0  ckPr0#4ckP  #4ckPr0#4c  r0#4     #4ckPr    
-    #4ckPr0   kPr0   kPr0  ckPr  4ckPr0#4   r0#4ckPr0#4  Pr0#    r0#4ckPr   
-   r0#4       ckPr   ckPr0#4ck    4c  r0#        ckPr    kPr0#4  Pr0# ckPr  
-   Pr0#       4ckP   4ckPr0#4c   0#4 kPr         4ckP    ckPr0# ckPr  4ckP  
-   kPr0      0#4ck    4ckPr0                    0#4ck     ckPr0 4ckP  #4ckP 
-               
-                         \033[0m  \033[91m    \033[1m 
+
+   ___                                                         
+  / _ \_ __ ___   __ _ _ __ __ _ _ __ ___  _ __ ___   ___ _ __ 
+ / /_)/ '__/ _ \ / _` | '__/ _` | '_ ` _ \| '_ ` _ \ / _ \ '__|
+/ ___/| | | (_) | (_| | | | (_| | | | | | | | | | | |  __/ |   
+\/    |_|  \___/ \__, |_|  \__,_|_| |_| |_|_| |_| |_|\___|_|   
+                 |___/                                         
+        	        _     _ _             _                        
+ 	_ __ _   _ ___| |__ (_) | _____  ___| |__                     
+	| '__| | | / __| '_ \| | |/ / _ \/ __| '_ \                    
+	| |  | |_| \__ \ | | | |   <  __/\__ \ | | |                   
+	|_|   \__,_|___/_| |_|_|_|\_\___||___/_| |_| 
+
+============================================================
+   ___       ___    _  _     _      ___       
+  / _ \_ __ / _ \ _| || |_  /_\    / __\ /\ /\
+ / /_)/ '__| | | |_  ..  _|//_\\  / /   / //_/
+/ ___/| |  | |_| |_      _/  _  \/ /___/ __ \ 
+\/    |_|   \___/  |_||_| \_/ \_/\____/\/  \/  \033[0m  \033[91m    \033[1m 
+
        }--{+} Created By Programmer Rushikesh {+}--{
      }----{+}  #4ck 4 Community {+}----{
        }--{+}  Even more .....  {+}--{                               
